63c8cfd7040859a83a345295bc6686a1db4bb8d6
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2011-07-07 14:11:07 +0200

    added Room implementation

---
+++ src/OpenSpacePlanner.Domain/Room.cs
@@ -0,0 +1,8 @@
+﻿using OpenSpacePlanner.Contracts;
+
+namespace OpenSpacePlanner.Domain {
+	public class Room : Entity, IRoom {
+		public string Name { get; private set; }
+		public int Capacity { get; private set; }
+	}
+}
