7f0932f335a98ec487af49c6f02395c6dde358b0
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2011-07-10 14:51:58 +0200

    Given_a_nos_session_repository_when_requesting_all_sessions => should_yield_all_sessions

--- src/OpenSpacePlanner.Repositories.Tests/NosSessionRepositorySpecs.cs
+++ src/OpenSpacePlanner.Repositories.Tests/NosSessionRepositorySpecs.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Machine.Fakes;
 using Machine.Specifications;
 using OpenSpacePlanner.Contracts;
@@ -43,6 +45,65 @@ namespace OpenSpacePlanner.Repositories.Tests
 		It should_insert_nos_session = () => { _actualNosSession.ShouldEqual(_expectedNosSession); };
 	}
 
+	public class Given_a_nos_session_repository_when_requesting_all_sessions : WithSubject<NosSessionRepository> {
+		static IEnumerable<INosSession> _actualSessions;
+		static INosSession _expectedNosSession1;
+		static INosSession _expectedNosSession2;
+		static INosSession _expectedNosSession3;
+
+		Establish context 
+			= () =>
+			  	{
+			  		With<NHibernateSqliteSessionProviderLoaded>();
+					_expectedNosSession1 = new NosSession() {
+						CreatedOn = DateTime.Now,
+						Title = "NHibernate in a NutShell",
+						Description = "Something on NHibernate",
+						Start = DateTime.Now + new TimeSpan(1, 0, 0),
+						End = DateTime.Now + new TimeSpan(2, 0, 0),
+						Owner = "Alexander Zeitler",
+						OwnerTag = "2arc",
+						Room = "3A",
+						Tag = "2seb"
+					};
+					_expectedNosSession2 = new NosSession() {
+						CreatedOn = DateTime.Now,
+						Title = "NHibernate in a NutShell",
+						Description = "Something on NHibernate",
+						Start = DateTime.Now + new TimeSpan(1, 0, 0),
+						End = DateTime.Now + new TimeSpan(2, 0, 0),
+						Owner = "Alexander Zeitler",
+						OwnerTag = "2arc",
+						Room = "3A",
+						Tag = "2seb"
+					};
+					_expectedNosSession3 = new NosSession() {
+						CreatedOn = DateTime.Now,
+						Title = "NHibernate in a NutShell",
+						Description = "Something on NHibernate",
+						Start = DateTime.Now + new TimeSpan(1, 0, 0),
+						End = DateTime.Now + new TimeSpan(2, 0, 0),
+						Owner = "Alexander Zeitler",
+						OwnerTag = "2arc",
+						Room = "3A",
+						Tag = "2seb"
+					};
+					using(var session = The<INHibernateSessionProvider>().GetSession()) {
+						session.Save(_expectedNosSession1);
+						session.Save(_expectedNosSession2);
+						session.Save(_expectedNosSession3);
+						session.Flush();
+					}
+			  	};
+
+		Because of = () =>
+		             	{
+		             		_actualSessions = Subject.Get();
+		             	};
+
+		It should_yield_all_sessions = () => { _actualSessions.Count().ShouldEqual(3); };
+	}
+
 	public class NosSessionRepository : INosSessionRepository {
 		readonly INHibernateSessionProvider _nHibernateSessionProvider;
 
@@ -54,6 +115,12 @@ namespace OpenSpacePlanner.Repositories.Tests
 			using(var session = _nHibernateSessionProvider.GetSession()) {
 				session.Save(nosSession);
 				session.Flush();
+			}
+		}
+
+		public IEnumerable<INosSession> Get() {
+			using(var session = _nHibernateSessionProvider.GetSession()) {
+				return session.CreateCriteria<NosSession>().List<NosSession>();
 			}
 		}
 	}
