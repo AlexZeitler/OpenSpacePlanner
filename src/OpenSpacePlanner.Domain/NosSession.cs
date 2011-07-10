3450f3d650c5f59a07086055a1656eaa21e1e98e
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2011-07-10 14:40:24 +0200

    Given_a_nos_session_repository_when_requesting_to_insert_a_nos_sessionNos => should_insert_nos_session

--- src/OpenSpacePlanner.Domain/NosSession.cs
+++ src/OpenSpacePlanner.Domain/NosSession.cs
@@ -3,14 +3,14 @@ using OpenSpacePlanner.Contracts;
 
 namespace OpenSpacePlanner.Domain {
 	public class NosSession : Entity, INosSession {
-		public virtual string Tag { get; private set; }
-		public virtual DateTime Start { get; private set; }
-		public virtual DateTime End { get; private set; }
-		public virtual string Title { get; private set; }
-		public virtual string Owner { get; private set; }
-		public virtual string OwnerTag { get; private set; }
-		public virtual string Description { get; private set; }
-		public virtual DateTime CreatedOn { get; private set; }
-		public virtual string Room { get; private set; }
+		public virtual string Tag { get; set; }
+		public virtual DateTime Start { get; set; }
+		public virtual DateTime End { get; set; }
+		public virtual string Title { get; set; }
+		public virtual string Owner { get; set; }
+		public virtual string OwnerTag { get; set; }
+		public virtual string Description { get; set; }
+		public virtual DateTime CreatedOn { get; set; }
+		public virtual string Room { get; set; }
 	}
 }
---
+++ src/OpenSpacePlanner.Repositories.Tests/NosSessionRepositorySpecs.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Machine.Fakes;
+using Machine.Specifications;
+using OpenSpacePlanner.Contracts;
+using OpenSpacePlanner.Domain;
+using OpenSpacePlanner.Repositories.Tests.Mappings;
+using PDMLab.Common.NHibernate;
+
+namespace OpenSpacePlanner.Repositories.Tests
+{
+	public class Given_a_nos_session_repository_when_requesting_to_insert_a_nos_sessionNos : WithSubject<NosSessionRepository>
+	{
+		static INosSession _expectedNosSession;
+		static INosSession _actualNosSession;
+
+		Establish context 
+			= () =>
+			  	{
+			  		With<NHibernateSqliteSessionProviderLoaded>();
+					_expectedNosSession = new NosSession()
+					                      	{
+					                      		CreatedOn = DateTime.Now,
+												Title = "NHibernate in a NutShell",
+												Description = "Something on NHibernate",
+												Start = DateTime.Now + new TimeSpan(1,0,0),
+												End = DateTime.Now + new TimeSpan(2,0,0),
+												Owner = "Alexander Zeitler",
+												OwnerTag = "2arc",
+												Room = "3A",
+												Tag = "2seb"
+					                      	};
+			  	};
+
+		Because of 
+			= () =>
+			  	{
+			  		Subject.Insert(_expectedNosSession);
+					using(var session = The<INHibernateSessionProvider>().GetSession()) {
+						_actualNosSession= session.Get<NosSession>(_expectedNosSession.Id);
+					}
+			  	};
+
+		It should_insert_nos_session = () => { _actualNosSession.ShouldEqual(_expectedNosSession); };
+	}
+
+	public class NosSessionRepository : INosSessionRepository {
+		readonly INHibernateSessionProvider _nHibernateSessionProvider;
+
+		public NosSessionRepository(INHibernateSessionProvider nHibernateSessionProvider) {
+			_nHibernateSessionProvider = nHibernateSessionProvider;
+		}
+
+		public void Insert(INosSession nosSession) {
+			using(var session = _nHibernateSessionProvider.GetSession()) {
+				session.Save(nosSession);
+				session.Flush();
+			}
+		}
+	}
+
+	public interface INosSessionRepository {
+	}
+}
