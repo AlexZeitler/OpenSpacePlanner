22279a3965574735f3f6f59a50b5e5e3c89d4598
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2011-07-14 10:22:35 +0200

    implemented index view for attendees

---
+++ src/OpenSpacePlanner.Web/Controllers/AttendeeAdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OpenSpacePlanner.Contracts;
+
+namespace OpenSpacePlanner.Web.Controllers
+{
+    public class AttendeeAdminController : Controller
+    {
+    	readonly IAttendeeRepository _attendeeRepository;
+
+    	public AttendeeAdminController(IAttendeeRepository attendeeRepository ) {
+    		_attendeeRepository = attendeeRepository;
+    	}
+
+    	//
+        // GET: /AttendeeAdmin/
+
+        public ActionResult Index() {
+        	var model = _attendeeRepository.Get().OrderBy(a=>a.LastName);
+            return View(model);
+        }
+
+    }
+}
--- src/OpenSpacePlanner.Web/Global.asax.cs
+++ src/OpenSpacePlanner.Web/Global.asax.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using FluentNHibernate.Cfg;
+using LightCore;
+using OpenSpacePlanner.Contracts;
+using OpenSpacePlanner.Repositories;
+using OpenSpacePlanner.Repositories.Mappings;
+using PDMLab.Common.NHibernate;
 
 namespace OpenSpacePlanner.Web {
 	// Note: For instructions on enabling IIS6 or IIS7 classic mode, 
 	// visit http://go.microsoft.com/?LinkId=9394801
 
 	public class MvcApplication : System.Web.HttpApplication {
+		static IContainer _container;
+
 		public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
 			filters.Add(new HandleErrorAttribute());
 		}
@@ -23,13 +31,23 @@ namespace OpenSpacePlanner.Web {
 				new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
 			);
 
+			ControllerBuilder.Current.SetControllerFactory(new LightCore.Integration.Web.Mvc.ControllerFactory(_container));
 		}
 
 		protected void Application_Start() {
 			AreaRegistration.RegisterAllAreas();
-
+			setupIoc();
 			RegisterGlobalFilters(GlobalFilters.Filters);
 			RegisterRoutes(RouteTable.Routes);
+		}
+
+		void setupIoc() {
+			Action<MappingConfiguration> mappingConfiguration =
+				mappings => mappings.FluentMappings.AddFromAssemblyOf<AttendeeMap>();
+			IContainerBuilder builder = new ContainerBuilder();
+			builder.Register<INHibernateSessionProvider, SqlServerConnectionStringNHibernateSessionProvider>().WithArguments("nosplanner", mappingConfiguration);
+			builder.Register<IAttendeeRepository, AttendeeRepository>();
+			_container = builder.Build();
 		}
 	}
 }
