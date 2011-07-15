9edadeb50024f7e2943fb999fe00580c1642dffc
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2011-07-15 10:00:42 +0200

    moved session javascript mostly into its own file

---
+++ src/OpenSpacePlanner.SessionWeb/Scripts/sessions.js
@@ -0,0 +1,30 @@
+﻿function refresh() {
+
+	$.ajax({
+			type: "POST",
+			url: "/Home/Samstag/",
+			dataType: 'html',
+			cache: false,
+			success: function(result) {
+				$('#samstag').empty();
+				$("#samstag").append(result);
+			},
+			error: function(error, response) {
+				alert(error.responseText);
+			}
+		});
+
+	$.ajax({
+			type: "POST",
+			url: "/Home/Sonntag/",
+			dataType: 'html',
+			cache: false,
+			success: function(result) {
+				$('#sonntag').empty();
+				$("#sonntag").append(result);
+			},
+			error: function(error, response) {
+				alert(error.responseText);
+			}
+		});
+}
